d817a9f9d1e0696c03579817920aa6d93b8f3ba7
XPauto <90854750+XPauto@users.noreply.github.com>
2022-01-25 19:37:48 -0500

    New Style Update! (MAJOR)
    First (pretty) major update!

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("Globe | A Scratch Alternative");
+window.addEventListener("load", (event) => {
+  console.log("Page loaded!");
+  document.getElementById("title").style.transform = "scale(1.15)";
+});
