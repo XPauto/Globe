1bdf86653ce3e9d79b1bd8e4ee42055670e30ba8
XPauto <90854750+XPauto@users.noreply.github.com>
2022-02-28 08:18:40 -0500

    deleted main js

--- main.js
+++
@@ -1,5 +0,0 @@
-console.log("Globe | A Scratch Alternative");
-window.addEventListener("load", (event) => {
-  console.log("Page loaded!");
-  document.getElementById("title").style.transform = "scale(1.15)";
-});
